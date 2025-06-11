b5c49dd86903ee607943c6f8a24a77a346e12ccb
Israel <jacobisraelreyessalas@gmail.com>
2025-06-10 19:51:18 -0600

    Se agregó la funcion de disponibilidad de horario de un medico

--- sql/vistas.sql
+++ sql/vistas.sql
@@ -1,10 +1,9 @@
-
 -- Sirve para relacionar a los medicos con las fechas de citas pendientes
 create view CitasMedico as
-	select us.Nombre+' '+us.Apellido_P+' '+us.Apellido_M as Medico, us.CURP, Email,
+	select med.ID_Medico,us.Nombre+' '+us.Apellido_P+' '+us.Apellido_M as Medico, us.CURP, Email,
 		esp.Nombre as Especialidad,
 		cit.Fecha_Cita,
-		hor.Inicio_Horario, hor.Fin_Horario
+		CONCAT( LEFT(hor.Inicio_Horario ,5) , ' - ',  LEFT(hor.Fin_Horario,5) ) AS Horario
 	from
 		Medico med left join Empleado emp on med.ID_Empleado = emp.ID_Empleado
 		left join Usuario us on emp.CURP = us.CURP
@@ -30,24 +29,22 @@ end
 
 -- Example: select dbo.obtenerEdadPaciente( cast( '2000-07-11' as date ) ) as Edad
 
-create view HorarioMedicosDisponible as
-	select
-		med.ID_Medico,
-		hor.Inicio_Horario,
-		hor.Fin_Horario
-	from
-	Medico med left join Cita cit on med.ID_Medico=cit.ID_Medico
-	left join Horario hor on cit.ID_Horario=hor.ID_Horario
-	where cit.Folio_Cita is not null
-
-	select * from Horario hor left join CitasMedico cm on hor.Inicio_Horario = cm.Inicio_Horario
-
 --vista Horarios
 
 CREATE VIEW HorariosDia
 AS
 SELECT ID_Horario AS ID, CONCAT( LEFT(Inicio_Horario,5) , ' - ',  LEFT(Fin_Horario,5) ) AS Horario FROM Horario WHERE ID_Horario <= 13
 
+
+create function obtenerDisponibilidadMedico( @idMedico int, @fecha date ) returns table
+as return (
+	select hd.Horario, 
+		case
+			when cm.Medico is null then 'Disponible'
+			else 'Ocupado'
+		end as Disponibilidad
+		from HorariosDia hd left join CitasMedico cm on hd.Horario=cm.Horario and ID_Medico=@idMedico and cm.Fecha_Cita=@fecha
+)
 
 --vista especialistas
 

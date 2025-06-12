0905fb671dba46d8f7a1e9df683d145cd28d5da2
Israel_PC_TRAVELING <jacobisraelreyessalas@gmail.com>
2025-06-12 08:55:59 -0600

    Se agrega la vista del historial de citas del paciente

--- sql/vistas.sql
+++ sql/vistas.sql
@@ -13,16 +13,27 @@ create view CitasMedico as
 		left join Horario hor on cit.ID_Horario = hor.ID_Horario
 			where cit.Folio_Cita is not null
 
-create view CitasPaciente as
-	select pac.ID_Paciente, us.Nombre+' '+us.Apellido_P+' '+us.Apellido_M as Paciente, us.CURP, Email,
-		cit.Folio_Cita,
-		cit.Fecha_Cita,
-		CONCAT( LEFT(hor.Inicio_Horario ,5) , ' - ',  LEFT(hor.Fin_Horario,5) ) AS Horario
-	from
-		Paciente pac left join Usuario us on pac.CURP = us.CURP
-		left join Cita cit on pac.ID_Paciente = cit.ID_Paciente 
-		left join Horario hor on cit.ID_Horario = hor.ID_Horario
-			where cit.Folio_Cita is not null
+create view HistorialCitasPaciente as
+	SELECT Cit.Fecha_Cita, Cit.ID_Paciente, Cit.Folio_Cita  AS Folio, 
+                    CONCAT(Us.Nombre, ' ', Us.Apellido_P) AS Medico,
+                    Esp.Nombre AS Especialidad,
+                    Cit.Fecha_Cita AS FechaCita, 
+                    CONCAT(
+                        LEFT(Ho.Inicio_Horario,5), ' - ',
+                        LEFT(Ho.Fin_Horario,5) )AS Horario,
+                    Cit.Fecha_Reservacion AS FechaRes, 
+                    Con.Numero AS NumConsultorio,
+                    Con.Piso AS PisoConsultorio,
+                    Fac.Estatus AS Estatus
+                FROM Cita Cit
+                LEFT JOIN Medico Med ON Cit.ID_Medico = Med.ID_Medico
+                LEFT JOIN Empleado Emp ON Med.ID_Empleado = Emp.ID_Empleado
+                LEFT JOIN Usuario Us ON Emp.CURP = Us.CURP
+                LEFT JOIN Horario Ho ON Cit.ID_Horario = Ho.ID_Horario
+                LEFT JOIN Especialidad Esp ON Med.ID_Especialidad = Esp.ID_Especialidad
+                LEFT JOIN Consultorio Con ON Cit.ID_Consultorio = Con.ID_Consultorio
+                LEFT JOIN Factura Fac ON Cit.ID_Factura = Fac.ID_Factura
+                
 
 create view DetallesCita as
 	select cp.Folio_Cita, Medico, Especialidad as EspecialidadDoctor, Paciente, cp.Fecha_Cita, cp.horario as Horario
